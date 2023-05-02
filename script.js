d3fb3a153e9d52bb74f494c9c69b586182c1d963
Prince Chaurasia <princechaurasiaofficial24@gmail.com>
2023-05-02 21:20:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// slider javascript
+
+const left = document.querySelector(".slider_button--left");
+const right = document.querySelector(".slider_button--right");
+
+const slide = document.querySelector(".slider-container");
+
+
+var count = 0;
+
+right.addEventListener("click", () => {
+    count = count - 320;
+    slide.style.left = count + "px";
+});
+
+
+left.addEventListener("click", () => {
+  count = count + 320;
+  slide.style.left = count + "px";
+});
+
+// slider js end
