b3136233ae181977d7c0f43eecb05ef83aa91689
Prince Chaurasia <princechaurasiaofficial24@gmail.com>
2023-05-19 21:44:03 +0530

    adding some js

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const right = document.querySelector(".slider_button--right");
 
 const slide = document.querySelector(".slider-container");
 
-
 var count = 0;
 
 right.addEventListener("click", () => {
-    count = count - 294;
-    slide.style.left = count + "px";
+  count = count - 294;
+  slide.style.left = count + "px";
 });
-
 
 left.addEventListener("click", () => {
   count = count + 294;
@@ -20,3 +18,16 @@ left.addEventListener("click", () => {
 });
 
 // slider js end
+
+// navbar activ page status
+
+const navelement = document.querySelectorAll(".header ul li");
+
+console.log(navelement);
+
+function active() {
+  navelement.forEach((i) => i.classList.remove("active"));
+  this.classList.add("active");
+}
+
+navelement.forEach((i) => i.addEventListener("click", active));
